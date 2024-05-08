a1ec5431f586cb1a24245d9b7194ca42eb69bb48
spark71 <noxdrummer@gmail.com>
2024-05-08 14:49:54 +0700

    add charts

--- app/app.py
+++ app/app.py
@@ -1,5 +1,5 @@
 from io import StringIO
-
+import altair as alt
 import streamlit as st
 import numpy as np
 import pandas as pd
@@ -16,7 +16,7 @@ import time
 
 st.header('🫀ЭКГ-сервис', divider='green')
 
-with st.expander('⬇️Загрузка сигнала'):
+with st.expander(':arrow_up:Загрузка сигнала'):
     st.markdown('''
         **Заполните предлагаемые поля данных. Далее загрузите сигнал.**
     ''')
@@ -25,33 +25,88 @@ with st.expander('⬇️Загрузка сигнала'):
     name = st.text_input("Имя пациента:")
     age = st.number_input("Возраст:", min_value=0, max_value=120, step=1)
     gender = option = st.selectbox("Пол:", ("М", "Ж"))
+    height = st.number_input("Рост:", min_value=0, max_value=300, step=1)
+    weight = st.number_input("Вес:", min_value=0, max_value=300, step=1)
+    date = str(st.date_input("Дата:"))
     device = st.text_input("Устройство:")
     uploaded_file = st.file_uploader("Выберите файл:", type=['txt', 'npy'])
-    # if uploaded_file is not None:
-    #     # To read file as bytes:
-    #     bytes_data = uploaded_file.getvalue()
-    #     st.write(bytes_data)
-    #
-    #     # To convert to a string based IO:
-    #     stringio = StringIO(uploaded_file.getvalue().decode("utf-8"))
-    #     st.write(stringio)
-    #
-    #     # To read file as string:
-    #     string_data = stringio.read()
-    #     st.write(string_data)
-    #
-    #     # Can be used wherever a "file-like" object is accepted:
-    #     dataframe = pd.read_csv(uploaded_file)
-    #     st.write(dataframe)
+    file_content = None
+    if uploaded_file:
+        file_content = np.loadtxt(StringIO(uploaded_file.getvalue().decode('utf-8')), dtype=float)
+    if (file_content is not None) and (file_content.shape == (1000, 12)):
+        st.success('Сигнал успешно загружен.', icon="✅")
+        success = True
+    else:
+        st.error('Файл не загружен. Пожалуйста, проверьте формат данных и потвторите попытку.', icon="🚨")
+        success = False
 
 if uploaded_file is not None:
     print(uploaded_file)
     print('FILE')
     print(type(uploaded_file.getvalue()))
+    print(type(file_content))
+    print(file_content, file_content.shape)
 
+if success:
+    with st.expander('📈Графики отведений'):
+        st.markdown('Выберите отведения.')
 
+        # ['I', 'II', 'III', 'AVR', 'AVL', 'AVF', 'V1', 'V2', 'V3', 'V4', 'V5', 'V6']
 
+        col1, col2, col3, col4 = st.columns(4)
+        with col1:
+            st.checkbox('I')
+            st.checkbox('II')
+            st.checkbox('III')
+        with col2:
+            st.checkbox('AVR')
+            st.checkbox('AVL')
+            st.checkbox('AVF')
+        with col3:
+            st.checkbox('V1')
+            st.checkbox('V2')
+            st.checkbox('V3')
+        with col4:
+            st.checkbox('V4')
+            st.checkbox('V5')
+            st.checkbox('V6')
 
+        st.button("Показать все отведения")
+        # Создаем DataFrame с данными для графика
+        df = pd.DataFrame({'x': np.arange(len(file_content[:, 0])), 'y': file_content[:, 0]})
 
+        # Создаем интерактивный график сигнала с помощью библиотеки Altair
+        # chart = alt.Chart(df).mark_line().encode(
+        #     x='x',
+        #     y='y',
+        #
+        # ).properties(
+        #     width=600, height=400
+        # ).interactive()
 
+        # Создаем интерактивный график сигнала с помощью библиотеки Altair
+        chart = alt.Chart(df).mark_line().encode(
+            x='x',
+            y='y'
+        )
+
+        # Наносим вертикальные линии
+        vertical_lines = alt.Chart(pd.DataFrame({'x': [10, 40, 70]})).mark_rule(color='red').encode(x='x')
+
+        # Совмещаем график и вертикальные линии
+        combined_chart = (chart + vertical_lines).properties(
+            width=600,  # задаем ширину графика
+            height=300  # задаем высоту графика
+        )
+
+        # Отображаем график в Streamlit
+        st.write(combined_chart)
+        # for lead in range(file_content.shape[0]-1):
+        #     # print(lead)
+        #     st.line_chart(file_content[:, lead-1], color="#f23c24")
+
+    with st.expander('🧾Диагностическая ифнормация'):
+        st.header('Общие сведения о сигнале.')
+        st.subheader("Параметры вариабельности сердечного ритма (ВСР)")
+        st.subheader("Классификация ЭКГ")
 

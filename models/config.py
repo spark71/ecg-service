3af2ab583399f16514ac0949d6e4d5490f50085d
spark71 <noxdrummer@gmail.com>
2024-06-22 15:24:33 +0700

    upd config

--- models/config.py
+++ models/config.py
@@ -1,22 +1,49 @@
+import torch
 from models.nn.xresnet1d import xresnet1d101
 from models.nn.inception1d import inception1d
 from models.nn.resnet1d import resnet1d_wang
 from models.nn.rnn1d import RNN1d
+from torch_ecg.utils.utils_nn import adjust_cnn_filter_lengths
+from torch_ecg.model_configs import ECG_CRNN_CONFIG
+from torch_ecg.models.ecg_crnn import ECG_CRNN
 
 def model_factory(model_name):
     model = None
     if model_name.lower()=='xresnet1d101':
         model = xresnet1d101(input_channels=12, num_classes=5)
 
-    if model_name.lower()=='resnet1d_wang':
-        model = resnet1d_wang(input_channels=12, num_classes=5)
+    if model_name.lower()=='resnet1d':
+        # model = resnet1d_wang(input_channels=12, num_classes=5)
+        resnet1d_wang_model = resnet1d_wang(input_channels=12, num_classes=5)
+        resnet1d_wang_weights = r'C:\Users\redmi\PycharmProjects\ecg-tool-api\models\pretrained\resnet1d_wang\resnet1d_wang_fold1_16epoch_best_score.pth'
+        # xresnet1d_model.load_state_dict(torch.load(xresnet1d_model_weights_path, map_location=torch.device('cpu'))['model'])
 
-    if model_name.lower()=='inception1d_model':
-        model = inception1d(input_channels=12, num_classes=5)
+        resnet1d_wang_model.load_state_dict(
+            torch.load(resnet1d_wang_weights, map_location=torch.device('cpu'))['model'])
+        model = resnet1d_wang_model.double().eval()
+        # resnet1d_wang_model
+
+    if model_name.lower()=='inception1d':
+        # model = inception1d(input_channels=12, num_classes=5)
+        inception1d_model = inception1d(input_channels=12, num_classes=5)
+        inception1d_model_weights_path = r'C:\Users\redmi\PycharmProjects\ecg-tool-api\models\pretrained\inception1d\inception1d_fold1_15epoch_best_score.pth'
+        inception1d_model.load_state_dict(
+            torch.load(inception1d_model_weights_path, map_location=torch.device('cpu'))['model'])
+        model = inception1d_model.double().eval()
+
 
     if model_name.lower()=='rnn_1d':
         model = RNN1d(input_channels=12, num_classes=5)
 
+    if model_name.lower() == 'vgg16':
+        vgg16_model_weights_path = r'C:\Users\redmi\PycharmProjects\ecg-tool-api\models\pretrained\vgg16\vgg16_fold1_10epoch_best_score.pth'
+        config = adjust_cnn_filter_lengths(ECG_CRNN_CONFIG, fs=100)
+        config.cnn.name = "vgg16"
+        classes = ['CD', 'HYP', 'MI', 'NORM', 'STTC']
+        n_leads = 12
+        vgg16_model = ECG_CRNN(classes, n_leads, config)
+        vgg16_model.load_state_dict(torch.load(vgg16_model_weights_path, map_location=torch.device('cpu'))['model'])
+        model = vgg16_model.double().eval()
     return model
 
 def load_pretrained_model(model_name):
@@ -68,7 +95,7 @@ class CqtCFG:
     trn_fold = [9]
     target_col = ['CD', 'HYP', 'MI', 'NORM', 'STTC']
     preds_col = ['pred_CD', 'pred_HYP', 'pred_MI', 'pred_NORM', 'pred_STTC']
-    model_name = 'tf_efficientnetv2_s'  # 'vit_base_patch32_224_in21k' 'resnext50_32x4d' 'tf_efficientnet_b3' 'resnetv2_50x1_bitm_in21k' 'inception_v4' 'tf_efficientnetv2_s_in21k'
+    model_name = 'tf_efficientnetv2_s'
     train = True
     grad_cam = False
     early_stop = True
@@ -77,9 +104,6 @@ class CqtCFG:
     scale = 30
     early_stopping_steps = 5
     seed = 42
-
-
-
 
 # if CqtCFG.debug:
 #     CqtCFG.epochs = 1
@@ -99,7 +123,7 @@ class RawCfg:
     train_folds = [1,2,3,4,5,6,7,8,9]
     target_col = ['CD', 'HYP', 'MI', 'NORM', 'STTC']
     preds_col = ['pred_CD', 'pred_HYP', 'pred_MI', 'pred_NORM', 'pred_STTC']
-    model_name = 'tf_efficientnetv2_s'  # 'vit_base_patch32_224_in21k' 'resnext50_32x4d' 'tf_efficientnet_b3' 'resnetv2_50x1_bitm_in21k' 'inception_v4' 'tf_efficientnetv2_s_in21k'
+    model_name = 'tf_efficientnetv2_s'
     train = True
     grad_cam = False
     early_stop = True
@@ -108,4 +132,10 @@ class RawCfg:
     scale = 30
     early_stopping_steps = 5
     seed = 42
+
+if __name__ == '__main__':
+    # model = model_factory("vgg16")
+    # model = model_factory("inception1d")
+    model = model_factory("resnet1d")
+    print(model)
 
